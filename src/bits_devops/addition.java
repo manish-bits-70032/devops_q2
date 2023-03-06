e75d274fbe2919333ea7941c694aa5c7a8172b79
manish-bits-70032 <2021mt70032@wilp.bits-pilani.ac.in>
2023-03-06 18:37:39 +0530

    addition of numbers

---
+++ src/bits_devops/addition.java
@@ -0,0 +1,13 @@
+package bits_devops;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =12;
+		int b = 13;
+		System.out.println(a+b);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author manipawa
+ *
+ */
+module bits {
+}
